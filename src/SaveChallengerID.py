cc0d255d095ec45553d79b3d7c1ccded622e2656
Remakaw <30518853+realuda@users.noreply.github.com>
2022-10-01 18:15:50 +0900

    Add files via upload
    request 오류 관련 수정, 챌+그마

--- src/SaveChallengerID.py
+++ src/SaveChallengerID.py
@@ -1,6 +1,7 @@
 #챌린저 유저 id 찾고 csv 파일로 저장
 import json
 import requests
+from requests.adapters import HTTPAdapter, Retry
 import pandas as pd
 import time
 
@@ -8,37 +9,79 @@ import time
 f = open("../key.txt",'r')
 api_key = f.readline()
 
-tier = 'CHALLENGER' #티어 
-tier2 = 'GRANDMASTER'
-
-URL = 'https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/'+tier+'/I'
-res = requests.get(URL, headers = {"X-Riot-Token": api_key })
+tier_list = ['CHALLENGER', 'GRANDMASTER']
 
 sidList = []
 sNameList = []
 puuidList = []
 
-if res.status_code == 200:
-    resobj = json.loads(res.text)
-    for i in resobj:
-        print(i["summonerName"])
-        sNameList.append(i["summonerName"])
-        sidList.append(i["summonerId"])
+#request보낼 때마다 +1
+count = 0
+err_count = list()
+
+#시작시간
+start = time.time()
+
+for tier in tier_list:
+    #티어에 따른 유저 정보(이름, sid)를 가져온다.
+    URL = 'https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/'+tier+'/I'
+    res = requests.get(URL, headers = {"X-Riot-Token": api_key })
+
+    count += 1
+
+    if res.status_code == 200:
+        
+        resobj = json.loads(res.text)
+        for i in resobj:
+            #print(i["summonerName"])
+            sNameList.append(i["summonerName"])
+            sidList.append(i["summonerId"])
+
+            #2분 최대 100회의 request만 수행한다
+            if count%100 == 0:
+                now_time = time.time()  #얼마나 지났나
+                time.sleep(120-(now_time-start)) #120초에서 경과시간을 뺀 만큼 sleep. 경과시간 = now_time-start
+                start = time.time() #start 다시 설정
+
+            try:
+                #request 초과 에러 방지용
+                session = requests.Session()
+                retry = Retry(connect = 10, backoff_factor=1)
+                adapter = HTTPAdapter(max_retries=retry)
+                session.mount('http://', adapter)
+                session.mount('https://', adapter)
+
+                #sid로 puuid가져오기. 2분 최대 100회
+                URL2 = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/"+i["summonerId"]
+                res2 = session.get(URL2, headers = {"X-Riot-Token": api_key})
+                #time.sleep(1.2)
+
+            except requests.exceptions.ConnectionError:
+                print('Connection error occured')
+
+            if res2.status_code == 200:   
+
+                challengerInfo = json.loads(res2.text)
+                #print(challengerInfo["puuid"])
+                
+                count+=1
+                print("Done "+str(count) + "/500")
+                puuidList.append(challengerInfo["puuid"])
+
+            else:
+                err_count.append(count)
+                count+=1
+                print("error occured. check your api key or input")
+
+    else:
+        print("error occured. check your api key or input")    
 
 
-        #puuid가져오기. 1분에 1600 requests
-        URL2 = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/"+i["summonerId"]
-        res2 = requests.get(URL2, headers = {"X-Riot-Token": api_key})
-        #time.sleep(1.2)
-        challengerInfo = json.loads(res2.text)
-        print(challengerInfo["puuid"])
-        puuidList.append(challengerInfo["puuid"])
+#저장
+df = pd.DataFrame(sNameList,columns = ['summonerName'])
+df['summonerId'] = sidList
+df['puuId'] = puuidList
 
-    df = pd.DataFrame(sNameList,columns = ['summonerName'])
-    df['summonerId'] = sidList
-    df['puuId'] = puuidList
+df.to_csv("ChallengerID.csv", index = False, encoding ='utf-8-sig')
 
-    df.to_csv("ChallengerID.csv", index = False, encoding ='utf-8-sig')
-
-else:
-    print("키가 잘못됨")    
+print(err_count)
--- src/SaveMatchID.py
+++ src/SaveMatchID.py
@@ -4,6 +4,7 @@ import requests
 import pandas as pd
 import time
 import datetime
+from requests.adapters import HTTPAdapter, Retry
 
 #api키 읽어오기
 f = open("../key.txt",'r')
@@ -25,14 +26,15 @@ wantAgo = str(int(current)-day_left) #원하는 날의 첫 날 timestamp
 df = pd.read_csv('challengerID.csv', encoding = 'utf-8-sig') #챌린저 유저 정보가 담긴 csv file read
 puuidList = df['puuId']
 
+#request보낼때마다 +1
+count = 0
+err_count = list()
+
+#시작시간
+start = time.time()
 
 #puuid에서 matchId 가져오기
-
-count=0
-
 for userPid in puuidList:
-
-
     tempDay = day_WantToGet #구하려는 날 임시 저장용 변수
     temp2 = 0 #며칠씩 가져올지 구하는 임시 변수
     tempAgo = wantAgo #남은 날짜 계산하는 임시 변수
@@ -44,11 +46,27 @@ for userPid in puuidList:
         else:
             temp2 = tempDay
 
+        #100번째마다 시간 검사
+        if count%100 == 0 and count != 0:
+            now_time = time.time()  #얼마나 지났나
+            time.sleep(120-(now_time-start)) #120초에서 경과시간을 뺀 만큼 sleep. 경과시간 = now_time-start
+            start = time.time() #start 다시 설정
 
-        #요청 URL. 최대 매치id를 100개밖에 못가져와서 3일씩 계산함
-        #10초에 500requests
-        URL = 'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{0}/ids?startTime={1}&endTime={2}&type=ranked&count=100'.format(userPid, tempAgo, str(int(tempAgo)+day*temp2))
-        res = requests.get(URL, headers = {"X-Riot-Token": api_key })
+        try:
+            #request 초과 에러 방지용
+            session = requests.Session()
+            retry = Retry(connect = 10, backoff_factor=1)
+            adapter = HTTPAdapter(max_retries=retry)
+            session.mount('http://', adapter)
+            session.mount('https://', adapter)
+
+            #요청 URL. 최대 매치id를 100개밖에 못가져와서 3일씩 계산함
+            #10초에 500requests
+            URL = 'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{0}/ids?startTime={1}&endTime={2}&type=ranked&count=100'.format(userPid, tempAgo, str(int(tempAgo)+day*temp2))
+            res = session.get(URL, headers = {"X-Riot-Token": api_key })
+
+        except requests.exceptions.ConnectionError:
+            print('Connection error occured')
 
         #성공
         if res.status_code == 200:
@@ -57,18 +75,23 @@ for userPid in puuidList:
             for i in matchId:
                 matchList.add(i)
 
-            count+=1
             tempDay = tempDay - temp2   #점점 3일씩 줄여나가다가 다 가져오면 끝
             tempAgo = str(int(tempAgo)+day*temp2)
+
+            count += 1
             print("Done " + str(count))
         
             #time.sleep(1.2) #한 번에 많은 요청을 보내면 거부해서 2초의 시간차를 둠
         #실패
         else:
-            print("URL 접근 실패")
+            err_count.append(count)
+            count += 1
+            print("error occured. check your api key or input")    
 
 
 
 #matchId csv로 저장
 df = pd.DataFrame(list(matchList), columns = ['matchId'])
 df.to_csv("ChallengerMatchID.csv", index = False, encoding = 'utf-8-sig')
+
+print(err_count)
